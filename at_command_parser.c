daed835d5531c89bc1c0e22794a359d01b7af493
avlad171 <blacklordakium@gmail.com>
2022-10-24 22:37:39 +0300

    Adjusted indentation

--- at_command_parser.c
+++ at_command_parser.c
@@ -1,192 +1,211 @@
 #include <stdint.h>
-#include <stdio.h>
 #include "at_command_parser.h"
+
 STATE_MACHINE_RETURN_VALUE at_command_parse(uint8_t current_character)
 {
 	static uint32_t state = 0;
+
 	switch (state){
-		 case 0:{
-			 if (current_character == 0x0D){
-			 	state = 1;
-			 }
-			 break;
-		 }
-		 case 1:{
-			 if (current_character == 0x0A){
-			 	state = 2;
-			 }
-			 else{
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
-			 break;
-		 }
-		 case 2:{
-			 if (current_character == 'O'){
-			 	state = 3;
-			 }
-			 else if (current_character == 'E'){
-			 	state = 4;
-			 }
-			 else if (current_character == '+'){
-			 	state = 5;
-			 }
-			 else{
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
-			break;
-		 }
+        case 0:{
+            if (current_character == 0x0D){
+                state = 1;
+            }
+            break;
+        }
+
+        case 1:{
+            if (current_character == 0x0A){
+                state = 2;
+            }
+            else{
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
+            break;
+        }
+
+        case 2:{
+            if (current_character == 'O'){
+                state = 3;
+            }
+            else if (current_character == 'E'){
+                state = 4;
+            }
+            else if (current_character == '+'){
+                state = 5;
+            }
+            else{
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
+            break;
+        }
+
 		case 3:{
-			 if (current_character == 'K'){
-			 	state = 6;
-			 }
-			 else{
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
+            if (current_character == 'K'){
+                state = 6;
+            }
+            else{
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
 			break;
-		 }
+        }
+
 		case 4:{
-			 if (current_character == 'R'){
-			 	state = 9;
-			 }
-			 else{
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
+            if (current_character == 'R'){
+                state = 9;
+            }
+            else{
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
 			break;
-		 }
+        }
+
 		case 5:{
-			if (current_character >= 32 && current_character <= 126){
-			 	state = 15;
-			 }
-			 else{
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
+            if (current_character >= 32 && current_character <= 126){
+                state = 15;
+            }
+            else{
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
 			break;
 		}
+
 		case 6:{
-			 if (current_character == 0x0D){
-			 	state = 7;
-			 }
-			 else{
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
+            if (current_character == 0x0D){
+                state = 7;
+            }
+            else{
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
 			break;
-		 }
+        }
+
 		case 7:{
-			 if (current_character == 0x0A){
-				return STATE_MACHINE_READY_OK;
-			 }
-			 else{
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
-			break;
-		 }
+            if (current_character == 0x0A){
+                return STATE_MACHINE_READY_OK;
+            }
+            else{
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
+        }
+
 		case 8:{
-			//ok final state
-			break;
-		 }
+            //ok final state
+            // break;
+        }
+
 		case 9:{
-			 if (current_character == 'R'){
+            if (current_character == 'R'){
 			 	state = 10;
-			 }
-			 else{
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
+            }
+            else{
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
 			break;
-		 }
+        }
+
 		case 10:{
-			 if (current_character == 'O'){
-			 	state = 11;
-			 }
-			 else{
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
-			break;
-		 }
+            if (current_character == 'O'){
+                state = 11;
+            }
+            else{
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
+            break;
+        }
+
 		case 11:{
-			 if (current_character == 'R'){
-			 	state = 12;
-			 }
-			 else{
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
+            if (current_character == 'R'){
+                state = 12;
+            }
+            else{
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
 			break;
-		 }
+        }
+
 		case 12:{
-			 if (current_character == 0x0D){
-			 	state = 13;
-			 }
-			 else{
+            if (current_character == 0x0D){
+                state = 13;
+            }
+            else{
 			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
-			break;
-		 }
+            }
+            break;
+        }
+
 		case 13:{
-			 if (current_character == 0x0A){
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
-			 else{
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
-			break;
-		 }
+            if (current_character == 0x0A){
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
+            else{
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
+        }
+
 		case 14:{
 			//error final state
-			break;
-		 }
+            break;
+        }
+
 		case 15:{
 			if (current_character >= 32 && current_character <= 126){
-			 	state = 15;
-			 }
+                state = 15;
+            }
 			else if(current_character == 0x0D){
-				state = 16;
+                state = 16;
 			}
-			 else{
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
+            else{
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
 			break;
 		}
+
 		case 16:{
-			 if (current_character == 0x0A){
-			 	state = 17;
-			 }
-			 else{
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
-			break;
-		 }
+            if (current_character == 0x0A){
+                state = 17;
+            }
+            else{
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
+            break;
+        }
+
 		case 17:{
 			if (current_character == '+'){
-			 	state = 5;
-			 }
-			 else if (current_character == 0x0D){
-			 	state = 18;
-			 }
-			 else{
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
+			    state = 5;
+            }
+            else if (current_character == 0x0D){
+                state = 18;
+            }
+            else{
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
 			break;
-		 }
+        }
+
 		case 18:{
-			if (current_character == 0x0A){
-			 	state = 19;
-			 }
-			 else{
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
-			break;
+            if (current_character == 0x0A){
+                state = 19;
+            }
+            else{
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
+            break;
 		}
+
 		case 19:{
 			if (current_character == 'O'){
-			 	state = 3;
-			 }
-			 else if (current_character == 'E'){
-			 	state = 4;
-			 }
-			 else{
-			 	return STATE_MACHINE_READY_WITH_ERROR;
-			 }
-			break;
-		 }
+                state = 3;
+            }
+            else if (current_character == 'E'){
+                state = 4;
+            }
+            else{
+                return STATE_MACHINE_READY_WITH_ERROR;
+            }
+            break;
+        }
 	}
+
 	return STATE_MACHINE_NOT_READY;
 } 
--- at_command_parser.h
+++ at_command_parser.h
@@ -1,4 +1,3 @@
-
 #ifndef __AT_COMMAND_PARSER_H
 #define __AT_COMMAND_PARSER_H
 
@@ -7,17 +6,18 @@
 
 typedef struct
 {
- uint8_t ok;
- uint8_t data[AT_COMMAND_MAX_LINES][AT_COMMAND_MAX_LINE_SIZE + 1];
- uint32_t line_count;
+    uint8_t ok;
+    uint8_t data[AT_COMMAND_MAX_LINES][AT_COMMAND_MAX_LINE_SIZE + 1];
+    uint32_t line_count;
 }AT_COMMAND_DATA; 
 
 typedef enum
 {
- STATE_MACHINE_NOT_READY,
- STATE_MACHINE_READY_OK,
- STATE_MACHINE_READY_WITH_ERROR
+    STATE_MACHINE_NOT_READY,
+    STATE_MACHINE_READY_OK,
+    STATE_MACHINE_READY_WITH_ERROR
 }STATE_MACHINE_RETURN_VALUE;
 
-STATE_MACHINE_RETURN_VALUE at_command_parse(uint8_t current_character); 
+STATE_MACHINE_RETURN_VALUE at_command_parse(uint8_t current_character);
+
 #endif
